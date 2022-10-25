4990882eb94cdd65d2bf3f5733ef6b03ea2aae1d
steever38 <53699839+steever38@users.noreply.github.com>
2022-10-25 20:59:24 +0200

    Add files via upload

---
+++ LEAKED - virus mail 2.0 OUVRE ça - LEAKED.bat
@@ -0,0 +1,52 @@
+start entrerl'emailquevousvoulerhacker.txt
+@echo off
+color 4
+echo ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo MMMMMMMMMMMMMMMMMMMMMMMMM         IIIIII                       IIIIII           OOOOOOOOOOOOOOOOOOO           OOOO                    OOOO         SSSSSSSSSSSSSSSSSSSSSSSSSSSS
+echo MMMMMMMMMMMMMMMMMMMMMMMMM         IIIIII                       IIIIII           OOOOOOOOOOOOOOOOOOO           OOOO                    OOOO        SSSSSSSSSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                                   OOOO                    OOOO       OOOO                    OOOO      SSSSSSSSSSSSSSSSSSSSS      SSS
+echo III                               IIIIII                                   OOOO                    OOOO       OOOO                    OOOO     SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO     SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO      SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO        SSSSSSSSSSSSSSSSSSSSS
+echo MMMMMMMMMMMMMMM                   IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO           SSSSSSSSSSSSSSSSSSSSS
+echo MMMMMMMMMMMMMMM                   IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO             SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO                SSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO               SSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO                SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO             SSSSSSSSSSSSSSSSSSSSS
+echo III                               IIIIII                       IIIIII      OOOO                    OOOO       OOOO                    OOOO          SSSSSSSSSSSSSSSSSSSSS
+echo MMMMMMMMMMMMMMMMMMMMMMMMM         IIIIIIIIIIIIIIIIIIIIIIII     IIIIII          OOOOOOOOOOOOOOOOOOOO           OOOOOOOOOOOOOOOOOOOOOOOOOOOO        SSSSSSSSSSSSSSSSSSSSS
+echo MMMMMMMMMMMMMMMMMMMMMMMMM         IIIIIIIIIIIIIIIIIIIIIIII     IIIIII          OOOOOOOOOOOOOOOOOOOO           OOOOOOOOOOOOOOOOOOOOOOOOOOOO    SSSSSSSSSSSSSSSSSSSSS
+
+
+                                                                                                                                                  
+                                                                                            
+echo --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+echo                                                          ce logiciel sert a inject des virus a des gens juste avec leur adress mail
+
+
+
+pause
+
+
+@echo off
+color d
+echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo PROXIES encours ...
+echo --------------------   
+
+
+
+matix.bat
+
+
+
+
+
